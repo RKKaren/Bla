fde01f72b5e4ee4b82ec28cf69a73421cfd15418
RKKaren <karen_simonyan_2001@mail.ru>
2017-05-19 18:42:05 +0400

    servernew

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 'use strict'
 
 const http = require('http');
-
-const server = http.createServer ((rq, res) =>{
-  if (rq.url == '/') {
-    res.setHeader('content-type',
-          'text/html'  );
-          res.end(`
-            <!doctype html>
-            <body>
-            <h1>Some header!</h1>
-            <p>what a body =P </p>
-            </body>
-            `)
-  } else {
-    res.end('UNKNOWN')
-  }
-
+const ports = require('./ports');
+const server = http.createServer((rq, res) => {
+    if (rq.url === '/') {
+      res.setHeader('content-type','text/html');
+      res.end(`
+        <!doctype html>
+        <body>
+        <h1>${ports.upper(__dirname)}</h1>
+        <p>what a body =P </p>
+        </body>
+      `)
+    } else {
+      res.end('UNKNOWN')
+    }
 })
 
-server.listen(9090,()=> console.log('Started our server'));
+server.listen(8080, () => console.log('Started our server'));
