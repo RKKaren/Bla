bf059d883bf96735f282ebacb73a6ccdb8d16412
RKKaren <karen_simonyan_2001@mail.ru>
2017-05-19 21:14:44 +0400

    socketjs

--- socket.js
+++ socket.js
@@ -3,21 +3,29 @@
 const net = require('net');
 const sock = new net.Socket;
 
+
 sock.on('connect', () => {
-  sock.write('Get/en/HTTP/1.1\r\n');
-  sock.write('Host:istc.am');
+  sock.write('GET /en/ HTTP/1.1 \r\n');
+  sock.write('Host: istc.am');
   sock.write('\r\n\r\n');
 });
 sock.setEncoding('utf-8');
-
 const total_data = [];
-
 sock.on('data', d => total_data.push(d));
+sock.on('end',() => {
+  const result =
+  total_data.reduce((prev, total) => prev + total);
+  const a = result.split('\n\r');
+  const head =  a[0].split('\r\n');
+  const obj = {
 
-sock.on('end',
-() => console.log (total_data.reduce((prev,total) => prev + total)));
+    headers: head,
+    body: a[1]
+  }
+  console.log(obj);
+})
 
 sock.connect({
   port: 80,
-  host:'istc.am'
+  host: 'istc.am'
 });
